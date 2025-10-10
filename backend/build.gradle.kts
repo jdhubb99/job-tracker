dd58eec1bede7870a8adcb30ee31b692f1db2eae
Jake Hubbard <jacobdhubbard@gmail.com>
2025-10-10 19:08:44 -0400

    chore: initial repo scaffold

---
+++ backend/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+	java
+	id("org.springframework.boot") version "3.5.6"
+	id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.jhub"
+version = "0.0.1-SNAPSHOT"
+description = "Backend for Job Tracker app"
+
+java {
+	toolchain {
+		languageVersion = JavaLanguageVersion.of(25)
+	}
+}
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-validation")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	runtimeOnly("org.postgresql:postgresql")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ backend/src/test/java/com/jhub/backend/JobtrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobtrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
