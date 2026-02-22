75e571ac2badb1033ea5e0e39b11e6ed0b31d0c6
Jake Hubbard <jacobdhubbard@gmail.com>
2026-02-22 15:16:54 -0500

    fix(backend): upgrade spotless and google-java-format

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
 	java
 	id("org.springframework.boot") version "4.0.2"
 	id("io.spring.dependency-management") version "1.1.7"
-	id("com.diffplug.spotless") version "7.0.2"
+	id("com.diffplug.spotless") version "8.2.1"
 }
 
 group = "com.jhub"
@@ -17,7 +17,7 @@ java {
 
 spotless {
 	java {
-		googleJavaFormat("1.27.0")
+		googleJavaFormat("1.34.1")
 		removeUnusedImports()
 		trimTrailingWhitespace()
 		endWithNewline()
